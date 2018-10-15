0ccc912ceccb296387b095fa83442c57aba4b030
panncherry <maung.eainmatmay@gmail.com>
2018-10-15 00:52:15 -0400

    added PanGestureRecognizer

--- Canvas/Canvas/CanvasViewController.swift
+++ Canvas/Canvas/CanvasViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class CanvasViewController: UIViewController {
 
     @IBOutlet weak var trayView: UIView!
+    var trayOriginalCenter: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
