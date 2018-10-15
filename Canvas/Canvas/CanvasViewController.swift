98cea8b9c21c37473bbf9b9c572366add38bc05d
panncherry <maung.eainmatmay@gmail.com>
2018-10-15 01:18:24 -0400

    added animation to trayView

--- Canvas/Canvas/CanvasViewController.swift
+++ Canvas/Canvas/CanvasViewController.swift
@@ -12,24 +12,55 @@ class CanvasViewController: UIViewController {
 
     @IBOutlet weak var trayView: UIView!
     var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    
+    @IBOutlet weak var downArrowImageView: UIImageView!
+    var isArrowFacingUp = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        trayDownOffset = 160
+        trayUp = trayView.center // The initial position of the tray
+        trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset) // The position of the tray transposed down
         // Do any additional setup after loading the view.
     }
     
     @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let velocity = sender.velocity(in: view)
+        let translation = sender.translation(in: view)
+        
+        if sender.state == .began {
+            print("Gesture began")
+            trayOriginalCenter = trayView.center
+        } else if sender.state == .changed {
+            print("Gesture is changing")
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            print("Gesture ended")
+            if velocity.y > 0 {
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayDown
+                }, completion: nil)
+            } else {
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayUp
+                }, completion: nil)
+            }
+            UIView.animate(withDuration:0.2, delay: 0.0,
+                           options: [], animations: { () -> Void in
+                            if self.isArrowFacingUp {
+                                self.downArrowImageView.transform = CGAffineTransform(rotationAngle: .pi)
+                            } else {
+                                self.downArrowImageView.transform = CGAffineTransform.identity
+                            }
+                            
+            }, completion: nil)
+        }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
